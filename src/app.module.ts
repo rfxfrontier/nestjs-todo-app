d9ae31fc39a65166202000f02e6b562225c0e2b0
rfxfrontier <9305356+rfxfrontier@users.noreply.github.com>
2024-02-28 01:34:21 +0800

    feat: init db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Logger, Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseRootModule } from './database/root/database.root.module';
 
 @Module({
-    imports: [TodoModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        DatabaseRootModule,
+        TodoModule,
+    ],
     controllers: [],
     providers: [Logger],
 })
