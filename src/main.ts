4fb68215d47ef55f09e49cde4b60ff0951f3d8e0
rfxfrontier <9305356+rfxfrontier@users.noreply.github.com>
2024-02-28 01:35:45 +0800

    feat: update starting service log

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    const logger: Logger = app.get(Logger);
-
-    const port = process.env.PORT || 3000
-    logger.log(`Starting service at port ${port}`)
+    const logger = new Logger('bootstrap');
+    const port = process.env.PORT || 3000;
+    logger.log(`Starting service at port ${port}`);
     await app.listen(port);
 }
 bootstrap();
