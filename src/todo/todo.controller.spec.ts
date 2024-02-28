b27874cb199e7693a419dc2e75cc3f864354d114
rfxfrontier <9305356+rfxfrontier@users.noreply.github.com>
2024-02-29 02:10:59 +0800

    refactor: test module import to support mock data source of typeorm

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -2,23 +2,36 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 import { Logger } from '@nestjs/common';
-import { TodoItemDbModule } from 'src/database/todo-item-db/todo-item-db.module';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { TodoItem } from 'src/dao/TodoItem';
-import { TodoItemRepositoryFake } from 'src/database/todo-item-db/todo-item-db.mock';
+import {
+    DataSourceMock,
+    TodoItemRepositoryMock,
+} from 'src/database/todo-item-db/todo-item-db.mock';
+import { DataSource } from 'typeorm';
+import { TodoItemDbService } from 'src/database/todo-item-db/todo-item-db.service';
 
 describe('TodoController', () => {
     let controller: TodoController;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [TodoItemDbModule],
+            imports: [],
             controllers: [TodoController],
-            providers: [TodoService, Logger],
-        })
-            .overrideProvider(getRepositoryToken(TodoItem))
-            .useValue(TodoItemRepositoryFake)
-            .compile();
+            providers: [
+                {
+                    provide: getRepositoryToken(TodoItem),
+                    useValue: TodoItemRepositoryMock,
+                },
+                {
+                    provide: DataSource,
+                    useValue: DataSourceMock,
+                },
+                Logger,
+                TodoItemDbService,
+                TodoService,
+            ],
+        }).compile();
 
         controller = module.get<TodoController>(TodoController);
     });
--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -4,19 +4,33 @@ import { Logger } from '@nestjs/common';
 import { TodoItemDbModule } from 'src/database/todo-item-db/todo-item-db.module';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { TodoItem } from 'src/dao/TodoItem';
-import { TodoItemRepositoryFake } from 'src/database/todo-item-db/todo-item-db.mock';
+import {
+    DataSourceMock,
+    TodoItemRepositoryMock,
+} from 'src/database/todo-item-db/todo-item-db.mock';
+import { DataSource } from 'typeorm';
+import { TodoItemDbService } from 'src/database/todo-item-db/todo-item-db.service';
 
 describe('TodoService', () => {
     let service: TodoService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [TodoItemDbModule],
-            providers: [TodoService, Logger],
-        })
-            .overrideProvider(getRepositoryToken(TodoItem))
-            .useValue(TodoItemRepositoryFake)
-            .compile();
+            imports: [],
+            providers: [
+                {
+                    provide: getRepositoryToken(TodoItem),
+                    useValue: TodoItemRepositoryMock,
+                },
+                {
+                    provide: DataSource,
+                    useValue: DataSourceMock,
+                },
+                Logger,
+                TodoItemDbService,
+                TodoService,
+            ],
+        }).compile();
 
         service = module.get<TodoService>(TodoService);
     });
