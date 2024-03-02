0a8a29b617f772aa46fa3e4e7b4543e50a6f8d35
rfxfrontier <9305356+rfxfrontier@users.noreply.github.com>
2024-03-03 00:30:55 +0800

    feat: init e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CustomValidationPipe } from 'src/core/custom-validation-pipe';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
+    let itemId = null
+
+    beforeAll(() => {
+        jest.useFakeTimers();
+        jest.setSystemTime(new Date(2024, 1, 1));
+    });
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +19,58 @@ describe('AppController (e2e)', () => {
         }).compile();
 
         app = moduleFixture.createNestApplication();
+        app.useGlobalPipes(new CustomValidationPipe());
         await app.init();
     });
 
-    // it('/ (GET)', () => {
-    //     return request(app.getHttpServer())
-    //         .get('/')
-    //         .expect(200)
-    //         .expect('Hello World!');
-    // });
+    it('/todo (POST)', async () => {
+        const payload = {
+            name: "test name",
+            description: "test description",
+            dueDateStr: "2024-03-06T16:00:00.000Z"
+        };
+        const test = request(app.getHttpServer())
+            .post('/todo')
+            .send(payload)
+            .expect(201);
+        itemId = (await test).body.itemId
+        console.log(`itemId ${itemId} for following tests`)
+        return test
+    });
+
+    it('/todo/search (POST)', async () => {
+        const payload = {
+            page: 1,
+            size: 5,
+            sortBy: 'CREATION_TIME_DESC'
+        };
+        return request(app.getHttpServer())
+            .post('/todo/search')
+            .send(payload)
+            .expect(200);
+    });
+
+    it('/todo/:id (GET)', async () => {
+        return request(app.getHttpServer())
+            .get(`/todo/${itemId}`)
+            .expect(200);
+    });
+
+    it('/todo/:id (PATCH)', async () => {
+        const payload = {
+            name: "test name - updated",
+            description: "test description - updated",
+            status: 1,
+        };
+        return request(app.getHttpServer())
+            .patch(`/todo/${itemId}`)
+            .send(payload)
+            .expect(200);
+    });
+
+    it('/todo/:id (DELETE)', async () => {
+        return request(app.getHttpServer())
+            .delete(`/todo/${itemId}`)
+            .expect(200);
+    });
 });
