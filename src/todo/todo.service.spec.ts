651490acadd8769d0078fd1168bbfe5d2d6990bd
rfxfrontier <9305356+rfxfrontier@users.noreply.github.com>
2024-03-02 23:39:26 +0800

    chore: remote import not in use

--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoService } from './todo.service';
 import { Logger } from '@nestjs/common';
-import { TodoItemDbModule } from 'src/database/todo-item-db/todo-item-db.module';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { TodoItem } from 'src/dao/TodoItem';
 import {
