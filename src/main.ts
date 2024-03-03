3f298c85bfb751d9424f74cb346548a903b05292
rfxfrontier <9305356+rfxfrontier@users.noreply.github.com>
2024-03-03 18:33:39 +0800

    feat: generate swagger document from application, formatting

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { CustomValidationPipe } from './core/custom-validation-pipe';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as fs from "fs";
+import * as fs from 'fs';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -20,8 +20,12 @@ async function bootstrap() {
         .build();
     const document = SwaggerModule.createDocument(app, config);
 
-    if (process.env.BUILD_SWAGGER_DOCUMENT === "BUILD_SWAGGER_DOCUMENT") {
-        fs.writeFileSync("./docs/swagger.json", JSON.stringify(document, null, 4), { encoding: 'utf8' });
+    if (process.env.BUILD_SWAGGER_DOCUMENT === 'BUILD_SWAGGER_DOCUMENT') {
+        fs.writeFileSync(
+            './docs/swagger.json',
+            JSON.stringify(document, null, 4),
+            { encoding: 'utf8' },
+        );
     }
     SwaggerModule.setup('api', app, document);
 
